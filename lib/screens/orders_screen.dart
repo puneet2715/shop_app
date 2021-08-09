569be83afbcf7c537850c7e4e58f8de4c205ba5a
Puneet <puneet2715@gmail.com>
2021-08-09 18:03:15 +0530

    better way for - working with orders on the web

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -5,38 +5,8 @@ import '../providers/orders.dart' show Orders;
 import '../widgets/order_Item.dart';
 import '../widgets/app_drawer.dart';
 
-class OrdersScreen extends StatefulWidget {
+class OrdersScreen extends StatelessWidget {
   static const routeName = '/orders';
-
-  @override
-  _OrdersScreenState createState() => _OrdersScreenState();
-}
-
-class _OrdersScreenState extends State<OrdersScreen> {
-  // var _isLoading = false;
-
-  @override
-  void initState() {
-    // Future.delayed(Duration.zero).then((_) async {
-    // setState(() {
-    //   _isLoading = true;
-    // });
-
-    // _isLoading = true;
-
-    // await Provider.of<Orders>(context, listen: false).fetchAndSetOrders(); //with listen: false, we can also make the methoda call without the Future.delayed(...) workaound
-
-    // Provider.of<Orders>(context, listen: false)
-    //     .fetchAndSetOrders()
-    //     .then((_) => {
-    //           setState(() {
-    //             _isLoading = false;
-    //           })
-    //         });
-
-    // });
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
