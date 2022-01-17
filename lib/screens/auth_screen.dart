d72d829a48ec7e6f03eec4700b3966e52130cf4a
Puneet <puneet2715@gmail.com>
2022-01-17 19:24:08 +0530

    animated container

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -106,23 +106,23 @@ class _AuthCardState extends State<AuthCard>
   };
   var _isLoading = false; //for loading spinner
   final _passwordController = TextEditingController();
-  AnimationController _controller;
-  Animation<Size> _heightAnimation;
+  // AnimationController _controller;
+  // Animation<Size> _heightAnimation;
 
   @override
   void initState() {
     super.initState();
-    _controller = AnimationController(
-      vsync: this,
-      duration: Duration(
-        milliseconds: 300,
-      ),
-    );
-    _heightAnimation = Tween<Size>(
-            begin: Size(double.infinity, 260), end: Size(double.infinity, 320))
-        .animate(
-      CurvedAnimation(parent: _controller, curve: Curves.bounceInOut),
-    );
+    // _controller = AnimationController(
+    //   vsync: this,
+    //   duration: Duration(
+    //     milliseconds: 300,
+    //   ),
+    // );
+    // _heightAnimation = Tween<Size>(
+    //         begin: Size(double.infinity, 260), end: Size(double.infinity, 320))
+    //     .animate(
+    //   CurvedAnimation(parent: _controller, curve: Curves.bounceInOut),
+    // );
     // _heightAnimation.addListener(() => setState(() {}));
   }
 
@@ -130,7 +130,7 @@ class _AuthCardState extends State<AuthCard>
   void dispose() {
     // TODO: implement dispose
     super.dispose();
-    _controller.dispose();
+    // _controller.dispose();
   }
 
   void _showErrorDialog(String message) {
@@ -203,12 +203,12 @@ class _AuthCardState extends State<AuthCard>
       setState(() {
         _authMode = AuthMode.Signup;
       });
-      _controller.forward();
+      // _controller.forward();
     } else {
       setState(() {
         _authMode = AuthMode.Login;
       });
-      _controller.reverse();
+      // _controller.reverse();
     }
   }
 
@@ -220,20 +220,17 @@ class _AuthCardState extends State<AuthCard>
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: AnimatedBuilder(
-        animation: _heightAnimation,
-        builder: (ctx, ch) => Container(
-        // height: _authMode == AuthMode.Signup ? 320 : 260,
-        height: _heightAnimation.value.height,
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 300),
+        curve: Curves.bounceOut,
+        height: _authMode == AuthMode.Signup ? 320 : 260,
+        // height: _heightAnimation.value.height,
         constraints:
-            // BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 320 : 260),
-            BoxConstraints(minHeight: _heightAnimation.value.height),
+            BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 320 : 260),
+        // BoxConstraints(minHeight: _heightAnimation.value.height),
         width: deviceSize.width * 0.75,
         padding: EdgeInsets.all(16.0),
-        child: ch),
-        // ------------------------------- IMPORTANT ---------------------------------
-        // the form below is not rebuilt this way but the container above in the builder is rebuilt   
-          child: Form(
+        child: Form(
           key: _formKey,
           child: SingleChildScrollView(
             child: Column(
